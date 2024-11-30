9417a7cfc8cbefe85be91955a256e0efe91f9e4f
1234 <abc@gmail.com>
2024-11-29 22:09:25 -0600

    first commit

---
+++ PeresonTesting.java
@@ -0,0 +1,11 @@
+/* testing for the person class */
+public class PeresonTesting {
+    public static void main(String [] args)
+    {
+        person pr = new person();
+        pr.setFirstName("Az");
+        pr.setLastName("Ar");
+        pr.setAge(21);
+        System.out.println(pr);
+    }
+}
---
+++ person.java
@@ -0,0 +1,51 @@
+/* gets a person first last nam and age */
+
+public class person {
+    private String firstName;
+    private String lastName;
+    int age;
+
+    // get and sets
+    // gets the first name
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    // get the last name
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    // gets the age 
+    public int getAge()
+    {
+        return age;
+    }
+
+    // sets the first name
+    public void setFirstName(String inFirstName)
+    {
+        firstName = inFirstName;
+    }
+
+    // set the last name
+    public void setLastName(String inLastName)
+    {
+        lastName = inLastName;
+    }
+
+    // sets the age
+    public void setAge(int inAge)
+    {
+        age = inAge;
+    }
+
+    // toString
+    public String toString()
+    {
+        String str;
+        str = "Firist Name: "+ firstName +"\n" + "Last Name: "+ lastName;
+        return str;
+    }
+}
