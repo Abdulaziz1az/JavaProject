c88884241a0c85315c5d52dde6f606a555e64210
1234 <abc@gmail.com>
2024-12-02 13:28:48 -0600

    I creat it all the getters

--- Student.java
+++ Student.java
@@ -3,9 +3,40 @@ public class Student {
     // class variables
    private String firstName;
    private String lastName;
-   private String stjatId;
+   private String stId; // star id
    private int year;
    private String major;
+
+   // gets and sets
+   private String getFirstName()
+   // returns the first name
+   {
+    return firstName;
+   }
+
+   private String getLastName()
+   // returns the last name
+   {
+    return lastName;
+   }
+
+   private String getStId()
+   // returns the star id
+   {
+    return stId;
+   }
+
+   private int getYear()
+   // returns the year 
+   {
+    return year;
+   }
+
+   private String getMajor()
+   // returns the major
+   {
+    return major;
+   }
    
 
 }
