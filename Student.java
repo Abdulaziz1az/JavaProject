bf39db08e44b68ba3439878c6412bb9e09d81803
1234 <abc@gmail.com>
2024-12-01 22:06:28 -0600

    A student class l set the class varibles

---
+++ Student.java
@@ -0,0 +1,11 @@
+// gets student information
+public class Student {
+    // class variables
+   private String firstName;
+   private String lastName;
+   private String stjatId;
+   private int year;
+   private String major;
+   
+
+}
