4e5fa41b2121557ce0dca43dd9e998e836c9a5ef
1234 <abc@gmail.com>
2024-12-06 13:05:10 -0600

    I set the main file

---
+++ StudentTesting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+// a main class for the student class
+import java.util.Scanner;
+
+public class StudentTesting {
+    public void main(String [] arsg)
+    {
+        Scanner Scannedinfo = new Scanner(System.in);
+        // Constructor
+        Student Student1 = new Student(); 
+    }
+}
