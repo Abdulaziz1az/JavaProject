30e51e7f697ed3ba1211776e64bb2a248b299d2f
1234 <abc@gmail.com>
2024-12-09 22:07:55 -0600

    Aske the use for  first name, last name and star id

--- StudentTesting.java
+++ StudentTesting.java
@@ -8,5 +8,17 @@ public class StudentTesting {
         Scanner Scannedinfo = new Scanner(System.in);
         // Constructor
         Student Student1 = new Student(); 
+        // aske  the user the user for the first name
+        System.out.print("Enter your first name: ");
+        Student1.setFirstName(Scannedinfo.nextLine());
+
+        // aske the user for last name
+        System.out.print("Enter your last name: ");
+        Student1.setLastName(Scannedinfo.nextLine());
+
+        // aske the user for star id
+        System.out.print("Enter your star id: ");
+        Student1.setStrId(Scannedinfo.nextLine());
+
     }
 }
