a7c08b51419fce214cf4eaf8743709a383ff4ec5
1234 <abc@gmail.com>
2024-12-16 19:28:21 -0600

    I declare  all the Variables and create all the gets

---
+++ Store/items.java
@@ -0,0 +1,24 @@
+// it is a class of items that returns and set the information
+public class items {
+    public String name; // the name of the item
+    public int price; 
+    public int quantity;
+
+    // gets and sets
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    
+}
