d6e57b88ae3a62b3c39880786539b0a0ab24ea65
Abdulaziz1az <152421200+Abdulaziz1az@users.noreply.github.com>
2024-12-20 10:35:15 -0600

    Add files via upload
    the main file for items class

---
+++ Store/itemsTesting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.concurrent.TransferQueue;
+public class itemsTesting {
+    public static void main(String [] arsg)
+    {
+        Scanner scannedinfo = new Scanner(System.in);
+        items iTems = new items();
+    // testing
+
+    }
+}
